a44cb02c20055fda717435b50c0c77d8d6cbcd71
Sylvia <sylviakieha@gmail.com>
2015-08-10 00:13:44 +0300

    if else statements

--- new.rb
+++ new.rb
@@ -7,3 +7,13 @@ puts 8 * 10
 puts 100 / 10
 puts 10 ** 4
 puts 15 % 4
+
+
+your_age = 120
+if your_age <= 17
+	puts "You are not old enough for this site"
+elsif your_age > 100
+	puts "Man you're old!"	
+else
+	puts "What is your gender?"
+end
