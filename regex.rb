b0db91f1357ae4f3797330a720f48d67cbf31043
Sylvia <sylviakieha@gmail.com>
2015-08-19 00:30:43 +0300

    using MatchData in regex

--- regex.rb
+++ regex.rb
@@ -10,3 +10,16 @@ end
 puts has_b?("beef")
 puts has_b?("bollocks")
 puts has_b?("make")
+
+#using MatchData object with regex
+def has_a_b? (string)
+  if /b/.match (string)
+    puts "matches"
+  else
+    puts "no match"
+  end
+end
+
+puts has_a_b?("beef")
+puts has_a_b?("bollocks")
+puts has_a_b?("make")
