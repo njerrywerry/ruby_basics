385ec75f3bbc178168105aa242005527b7e60213
Sylvia <sylviakieha@gmail.com>
2015-08-19 08:38:26 +0300

    more on usage of blocks

--- blocks2.rb
+++ blocks2.rb
@@ -6,3 +6,14 @@ end
 my_block do
   puts "this is my new block"
 end
+
+#passing an arguement into a block method
+def take_block (num, &block)
+  block.call
+end
+
+[1, 2, 3, 4, 5].each do |num|
+ take_block num do
+  puts "blocks being called #{num}"
+ end
+end
