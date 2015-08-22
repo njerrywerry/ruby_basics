7c08a1f2d412196a535e0ac6a187a4d3c8296204
Sylvia <sylviakieha@gmail.com>
2015-08-22 15:43:19 +0300

    passing a proc in a method

--- procs.rb
+++ procs.rb
@@ -11,3 +11,18 @@ greet = Proc.new do |name|
 end
 
 greet.call("Njerry")
+
+#passing a proc in a method
+def notify proc
+  puts "You have a notification!"
+  proc.call
+end
+notification = Proc.new do
+puts "Your battery is below 15%"
+end
+notification2 = Proc.new do
+  puts "Battery full"
+end
+
+notify notification
+notify notification2
