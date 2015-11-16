b4d3beb7838161f9139cba31cfa0e180dca3d9ea
Sylvia <sylviakieha@gmail.com>
2015-11-16 13:49:00 +0300

    more on user input

--- user_input.rb
+++ user_input.rb
@@ -1,4 +1,9 @@
-print "Please type your first name."
+puts "Please type your first name."
 user_input = gets.chomp.capitalize
 puts "Your first name is #{user_input.upcase}"
 puts "Your name proper is #{user_input}"
+
+puts "Please input your age"
+age = gets.chomp
+
+puts "you are #{age} years old"
